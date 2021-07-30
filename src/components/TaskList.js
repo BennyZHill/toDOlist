aa4a42fa2ff5e92771888e716579df2b0d8324f3
BennyZHill <ben.h@builtbypixel.com>
2021-07-30 14:19:12 +0100

    commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -15,12 +15,12 @@ function TaskList() {
     setTasks(newTasks);
   };
 
-  const updateTask = (todoId, newValue) => {
+  const updateTask = (taskId, newValue) => {
     if (!newValue.text || /^\s*$/.test(newValue.text)) {
       return;
     }
 
-    setTasks(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    setTasks(prev => prev.map(item => (item.id === taskId ? newValue : item)));
   };
 
   const removeTask = id => {
