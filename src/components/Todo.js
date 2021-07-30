db1fbcd49349a2ce0c805c2f8ca534e8278a477e
BennyZHill <ben.h@builtbypixel.com>
2021-07-30 11:08:06 +0100

    added stylecomponensts

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
-import { RiCloseCircleLine } from 'react-icons/ri';
+import {RiCloseCircleLine} from 'react-icons/ri';
 import {TiEdit} from 'react-icons/ti';
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
